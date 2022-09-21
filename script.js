f866a027c99709ef8a9bbe09e8b0fa5eed0d9903
Pete <63628694+peterekey@users.noreply.github.com>
2022-09-21 13:53:40 +1000

    Add front-end and link script to front-end.

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const messages = [
     "Why did I pick today, of all days, to turn it this hard?",
     "Boots. Boots, tea. Honey, boots and tea.",
     "And I said, 'Congressman! Is that your wife? By the trash? Gobbling up submarine sandwiches?'",
-    "I was described as a tank barreling down a muddy hill that could be derailed by a twig",
-    "You can't judge a fish by its ability to transcribe",
-    "And you know what the lesson was? Everybody's different, unhhhh",
+    "I was described as a tank barreling down a muddy hill that could be derailed by a twig.",
+    "You can't judge a fish by its ability to transcribe.",
+    "And you know what the lesson was? Everybody's different.",
     "What's the matter? You never seen a 50-year-old man before?",
     "Tire rotation. Four wheel drive.",
     "You look like a potholder.",
     "I'm in my 20's...... UNHHHHH 20'ssssss!",
-    "If the lord can lead you to it, he can lead you through it"
+    "If the lord can lead you to it, he can lead you through it."
 ];
 
-let selectedIndex = Math.floor(Math.random() * messages.length);
-
-console.log(messages[selectedIndex]);
+const button = document.querySelector("#quote-button");
+const box = document.querySelector("#quote");
+button.addEventListener("click", (e) => {
+    let selectedIndex = Math.floor(Math.random() * messages.length);
+    box.innerText = messages[selectedIndex];
+});
